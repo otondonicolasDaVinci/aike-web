b7113e7fa8c79c37be1feb1979f88f3ae4dfcc5c
otondonicolasDaVinci <109868679+otondonicolasDaVinci@users.noreply.github.com>
2025-07-13 17:17:41 -0300

    Remove Navbar component and restore inline navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
 import BackButton from './BackButton';
 
 function Layout() {
     return (
         <>
-            <Navbar />
             <BackButton />
             <div className="pt-16">
                 <Outlet />
